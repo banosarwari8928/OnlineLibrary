ef05e546866148e0877d3c9b8e5ad7a0a391a6e8
banosarwari8928 <banosarwari8928@gmail.com>
2026-02-05 12:45:55 +0430

    model and factroy added for books

--- database/migrations/2026_02_05_080212_create_books_table.php
+++ database/migrations/2026_02_05_080212_create_books_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('authors', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
-            $table->text("bio")->nullable();
-            $table->string("nationality")->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('authors');
+        Schema::dropIfExists('books');
     }
 };
