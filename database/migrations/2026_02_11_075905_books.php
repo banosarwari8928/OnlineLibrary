47d85f7d72b3279bcd78f0a6482549b5238711d1
banosarwari8928 <banosarwari8928@gmail.com>
2026-02-11 12:29:18 +0430

    worked on solving some errors

--- database/migrations/2026_02_11_075905_books.php
+++ database/migrations/2026_02_11_075905_books.php
@@ -12,13 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         //
-         Schema::create('authors', function (Blueprint $table) {
-            $table->id();
-            $table->string("name");
-            $table->text("bio")->nullable();
-            $table->string("nationality")->nullable();
-    });
     }
+
     /**
      * Reverse the migrations.
      */
