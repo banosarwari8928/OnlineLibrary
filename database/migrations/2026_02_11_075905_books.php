ea7cf3018ef23e11e97624a1a253cb26cdec60bb
banosarwari8928 <banosarwari8928@gmail.com>
2026-02-11 12:31:16 +0430

    worked on adding columns to some tables

--- database/migrations/2026_02_11_075905_books.php
+++ database/migrations/2026_02_11_075905_books.php
@@ -12,6 +12,23 @@ return new class extends Migration
     public function up(): void
     {
         //
+         Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("isbn");
+            $table->text("disciption")->nullable();
+            $table->date("published_at");
+            $table->integer("total_copies")->defualt(1);
+            $table->integer("availabel_copies")->defualt(1);
+            $table->string("cover_image");
+            $table->enum("status", ["available", "unavialable"])->defualt("available");
+            $table->decimal("price" , 4,2);
+            $table->foreignId("author_id")->constrained("author")->cascadeOnDelete();
+            $table->string("genra");
+            $table->timestamps();
+            $table->index(["title","author_id"]);
+            $table->index(["isbn"]);
+        });
     }
 
     /**
