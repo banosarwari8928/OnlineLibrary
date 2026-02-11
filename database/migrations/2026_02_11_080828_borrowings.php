ebd6053487bf711a3ae1511989f81b1f4c974558
banosarwari8928 <banosarwari8928@gmail.com>
2026-02-11 12:39:59 +0430

    some changes added

--- database/migrations/2026_02_11_080828_borrowings.php
+++ database/migrations/2026_02_11_080828_borrowings.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         //
-        Schema::create('borrowing', function (Blueprint $table) {
+        Schema::create('borrowings', function (Blueprint $table) {
             $table->id();
             $table->foreignId("book_id")->constrained("books")->cascadeOnDelete();
             $table->foreignId("member_id")->constrained("members")->cascadeOnDelete();
