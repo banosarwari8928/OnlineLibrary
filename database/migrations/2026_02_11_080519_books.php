af2bd5f5f9795796004888835ab9a97859112c8b
banosarwari8928 <banosarwari8928@gmail.com>
2026-02-11 12:36:35 +0430

    some changes added

--- database/migrations/2026_02_11_080519_books.php
+++ database/migrations/2026_02_11_080519_books.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         //
-         Schema::create('books', function (Blueprint $table) {
+          Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string("title");
             $table->string("isbn");
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string("cover_image");
             $table->enum("status", ["available", "unavialable"])->defualt("available");
             $table->decimal("price" , 4,2);
-            $table->foreignId("author_id")->constrained("author")->cascadeOnDelete();
+            $table->foreignId("author_id")->constrained("authors")->cascadeOnDelete();
             $table->string("genra");
             $table->timestamps();
             $table->index(["title","author_id"]);
